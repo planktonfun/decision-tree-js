8766d17f62d5c05c25b235610b95eb9239fe167a
Paulo Castro <paulo.castro@honcho.com>
2017-12-30 01:48:43 +0800

    added input

--- js-code-demo/script.js
+++ js-code-demo/script.js
@@ -1,42 +1,17 @@
-// Training set
-var data = 
-    [{person: 'Homer', hairLength: 0, weight: 250, age: 36, sex: 'male'},
-     {person: 'Marge', hairLength: 10, weight: 150, age: 34, sex: 'female'},
-     {person: 'Bart', hairLength: 2, weight: 90, age: 10, sex: 'male'},
-     {person: 'Lisa', hairLength: 6, weight: 78, age: 8, sex: 'female'},
-     {person: 'Maggie', hairLength: 4, weight: 20, age: 1, sex: 'female'},
-     {person: 'Abe', hairLength: 1, weight: 170, age: 70, sex: 'male'},
-     {person: 'Selma', hairLength: 8, weight: 160, age: 41, sex: 'female'},
-     {person: 'Otto', hairLength: 10, weight: 180, age: 38, sex: 'male'},
-     {person: 'Krusty', hairLength: 6, weight: 200, age: 45, sex: 'male'}];
+$(document).on('click','#submit-button',function(){
+    var inputArea = $('#input-area').val().trim();
+    var inputArea2 = $('#input-area-2').val().trim();
+    var data = JSON.parse('[' + inputArea.replace(/[\s]/g,"").replace(/([\w]+):/g,"\"$1\":").replace(/:'([\w]+)'/g,":\"$1\"") + ']');
+    var config = JSON.parse(inputArea2.replace(/[\s]/g,"").replace(/([\w]+):/g,"\"$1\":").replace(/:'([\w]+)'/g,":\"$1\""));
+    config.trainingSet = data;
 
-// Configuration
-var config = {
-    trainingSet: data, 
-    categoryAttr: 'sex', 
-    ignoredAttributes: ['person']
-};
+    // Building Decision Tree
+    var decisionTree = new dt.DecisionTree(config);
 
-// Building Decision Tree
-var decisionTree = new dt.DecisionTree(config);
+    $('#displayTree').html(treeToHtml(decisionTree.root));
 
-// Building Random Forest
-var numberOfTrees = 3;
-var randomForest = new dt.RandomForest(config, numberOfTrees);
+});
 
-// Testing Decision Tree and Random Forest
-var comic = {person: 'Comic guy', hairLength: 8, weight: 290, age: 38};
-
-var decisionTreePrediction = decisionTree.predict(comic);
-var randomForestPrediction = randomForest.predict(comic);
-
-// Displaying predictions
-document.getElementById('testingItem').innerHTML = JSON.stringify(comic, null, 0);
-document.getElementById('decisionTreePrediction').innerHTML = JSON.stringify(decisionTreePrediction, null, 0);
-document.getElementById('randomForestPrediction').innerHTML = JSON.stringify(randomForestPrediction, null, 0);
-
-// Displaying Decision Tree
-document.getElementById('displayTree').innerHTML = treeToHtml(decisionTree.root);
 
 
 // Recursive (DFS) function for displaying inner structure of decision tree
